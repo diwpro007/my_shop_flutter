0338fefba3d976da943aa3cfdde58f9e10478482
diwpro007 <diwserala@gmail.com>
2020-10-30 09:21:50 +0545

    Filtering fav items logic

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,7 +16,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -35,8 +35,10 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(
-                product.isFavourite ? Icons.favorite : Icons.favorite_border),
+            icon: Consumer<Product>(
+              builder: (context, value, child) => Icon(
+                  product.isFavourite ? Icons.favorite : Icons.favorite_border),
+            ),
             color: Theme.of(context).accentColor,
             onPressed: () {
               product.toggleFavouriteStatus();
